dfea7e9601f5f061556dd3fec204c9aca5cfc383
Aditya Verma <aaadarths2357@gmail.com>
2020-09-25 19:02:56 +0530

    Welcome to User Registration System

---
+++ src/main/java/com/bridgelabz/pattern/userregistration/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.pattern.userregistration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Welcome to User Registration System");
+    }
+}
