8e50f5afbba653a7651906add2928706db2244e3
Aditya Verma <aaadarths2357@gmail.com>
2020-09-26 16:51:51 +0530

    Use Case 1 : Validate First Name

--- src/main/java/com/bridgelabz/pattern/userregistration/App.java
+++ src/main/java/com/bridgelabz/pattern/userregistration/App.java
@@ -1,9 +1,23 @@
 package com.bridgelabz.pattern.userregistration;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class App 
 {
+	
+	public boolean validateFirstName(String firstName) {
+		String firstNamePattern ="^[A-Z]{1}[a-zA-Z]{2,}";
+		boolean isValidFirstName=firstName.matches(firstNamePattern);
+		return isValidFirstName;
+	}
     public static void main( String[] args )
     {
-    	System.out.println("Welcome to User Registration System");
+    	App userObject=new App();
+    	Scanner sc=new Scanner(System.in);
+    	    	
+    	System.out.println("Enter First Name : ");
+    	String firstName=sc.next();
+    	boolean satisfiesFirstName=userObject.validateFirstName(firstName);
+    	System.out.println("Satisfies First Name : "+satisfiesFirstName);
     }
 }
