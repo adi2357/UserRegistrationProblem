fdf8b12e61d1db51e8f2080a94efe33f45749eb4
Aditya Verma <aaadarths2357@gmail.com>
2020-09-26 16:53:08 +0530

    Use Case 2 : Validate Last Name

--- src/main/java/com/bridgelabz/pattern/userregistration/App.java
+++ src/main/java/com/bridgelabz/pattern/userregistration/App.java
@@ -10,14 +10,25 @@ public class App
 		boolean isValidFirstName=firstName.matches(firstNamePattern);
 		return isValidFirstName;
 	}
+	
+	public boolean validateLastName(String lastName) {
+		String lastNamePattern ="^[A-Z]{1}[a-zA-Z]{2,}";
+		boolean isValidLastName=lastName.matches(lastNamePattern);
+		return isValidLastName;
+	}
     public static void main( String[] args )
     {
     	App userObject=new App();
     	Scanner sc=new Scanner(System.in);
     	    	
-    	System.out.println("Enter First Name : ");
-    	String firstName=sc.next();
-    	boolean satisfiesFirstName=userObject.validateFirstName(firstName);
-    	System.out.println("Satisfies First Name : "+satisfiesFirstName);
+//    	System.out.println("Enter First Name : ");
+//    	String firstName=sc.next();
+//    	boolean satisfiesFirstName=userObject.validateFirstName(firstName);
+//    	System.out.println("Satisfies First Name : "+satisfiesFirstName);
+    	
+    	System.out.println("Enter Last Name : ");
+    	String lastName=sc.next();
+    	boolean satisfiesLastName=userObject.validateLastName(lastName);
+    	System.out.println("Satisfies Last Name : "+satisfiesLastName);
     }
 }
